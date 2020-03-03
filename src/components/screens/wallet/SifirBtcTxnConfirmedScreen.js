4ba2dcf9c455656eaf06dfff15075ed85e5c9d55
hamzawaleed0102 <hamzawaleed0102@gmail.com>
2020-03-03 12:59:53 +0500

    Integrate ln.payBolt action

--- src/components/screens/wallet/SifirBtcTxnConfirmedScreen.js
+++ src/components/screens/wallet/SifirBtcTxnConfirmedScreen.js
@@ -10,62 +10,81 @@ import {
 } from 'react-native';
 import {connect} from 'react-redux';
 import {Images, AppStyle, C} from '@common/index';
+import {payBolt} from '@actions/lnWallet';
 
 class SifirBtcTxnConfirmedScreen extends Component {
   constructor(props, context) {
     super(props, context);
   }
+  componentDidMount() {
+    if (this.props.route.params.type === 'lnInvoice') {
+      console.log('lnInvoice in CDM------');
+      this.payBolt();
+    }
+  }
+
+  payBolt = async () => {
+    const {bolt11, route} = this.props.route.params;
+    console.log('calling payBolt() with param', {bolt11, route});
+    const info = await this.props.payBolt(bolt11, route);
+    console.warn('payBolt() response----------', info);
+  };
+
   done = () => {
     const {walletInfo} = this.props.route.params;
     this.props.navigation.navigate('Account', {walletInfo});
   };
 
   render() {
-    const {
-      txnInfo: {amount, address, isSendTxn},
-    } = this.props.route.params;
+    // const {
+    //   txnInfo: {amount, address, isSendTxn},
+    // } = this.props.route.params;
     // TODO remove from here
-    const {loaded, loading, btcSendResult} = this.props.btcWallet;
-    const addrTxtFontSize = (C.vw * 250) / address.length;
+    // const {loaded, loading, btcSendResult} = this.props.btcWallet;
+    // const addrTxtFontSize = (C.vw * 250) / address.length;
+
+    // FIXME uncomment and fix the below code once done with payBolt() action.
+
     return (
-      <View style={styles.mainView}>
-        {loading === true && (
-          <View style={styles.loading}>
-            <ActivityIndicator size="large" color={AppStyle.mainColor} />
-          </View>
-        )}
-        {loaded === true && btcSendResult !== null && (
-          <>
-            <View style={{alignItems: 'center', flex: 3}}>
-              <Image source={Images.icon_done} style={styles.checkImg} />
-              <Text style={styles.paymentTxt}>{C.STR_PAYMENT}</Text>
-              <Text style={styles.addressLblTxt}>
-                {isSendTxn ? C.STR_SENT : C.STR_RECEIVED}
-              </Text>
-              <Text style={styles.payAddrTxt}>
-                {C.STR_PAYMENT +
-                  ' ' +
-                  (isSendTxn ? C.STR_RECEIPIENT : C.STR_SENDER)}
-              </Text>
-              <Text style={[styles.addrTxt, {fontSize: addrTxtFontSize}]}>
-                {address}
-              </Text>
-              <Text style={styles.amountLblTxt}>{C.STR_PAYMENT_AMOUNT}</Text>
-              <Text style={styles.amountTxt}>{amount + ' ' + C.STR_BTC}</Text>
-            </View>
-            <TouchableOpacity
-              style={styles.doneTouch}
-              onPressOut={() => this.done()}>
-              <View
-                shadowColor="black"
-                shadowOffset="30"
-                style={styles.doneView}>
-                <Text style={styles.doneTxt}>{C.STR_DONE}</Text>
-              </View>
-            </TouchableOpacity>
-          </>
-        )}
-      </View>
+      <View />
+      // <View style={styles.mainView}>
+      //   {loading === true && (
+      //     <View style={styles.loading}>
+      //       <ActivityIndicator size="large" color={AppStyle.mainColor} />
+      //     </View>
+      //   )}
+      //   {loaded === true && btcSendResult !== null && (
+      //     <>
+      //       <View style={{alignItems: 'center', flex: 3}}>
+      //         <Image source={Images.icon_done} style={styles.checkImg} />
+      //         <Text style={styles.paymentTxt}>{C.STR_PAYMENT}</Text>
+      //         <Text style={styles.addressLblTxt}>
+      //           {isSendTxn ? C.STR_SENT : C.STR_RECEIVED}
+      //         </Text>
+      //         <Text style={styles.payAddrTxt}>
+      //           {C.STR_PAYMENT +
+      //             ' ' +
+      //             (isSendTxn ? C.STR_RECEIPIENT : C.STR_SENDER)}
+      //         </Text>
+      //         <Text style={[styles.addrTxt, {fontSize: addrTxtFontSize}]}>
+      //           {address}
+      //         </Text>
+      //         <Text style={styles.amountLblTxt}>{C.STR_PAYMENT_AMOUNT}</Text>
+      //         <Text style={styles.amountTxt}>{amount + ' ' + C.STR_BTC}</Text>
+      //       </View>
+      //       <TouchableOpacity
+      //         style={styles.doneTouch}
+      //         onPressOut={() => this.done()}>
+      //         <View
+      //           shadowColor="black"
+      //           shadowOffset="30"
+      //           style={styles.doneView}>
+      //           <Text style={styles.doneTxt}>{C.STR_DONE}</Text>
+      //         </View>
+      //       </TouchableOpacity>
+      //     </>
+      //   )}
+      // </View>
     );
   }
 }
@@ -76,7 +95,7 @@ const mapStateToProps = state => {
   };
 };
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = {payBolt};
 
 export default connect(
   mapStateToProps,
