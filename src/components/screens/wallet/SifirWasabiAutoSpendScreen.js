5e2d32e8b4271f2d0fa3d2dcfb959f572bcd9c3c
Hamza Waleed <hamzawaleed0102@gmail.com>
2020-06-12 00:52:00 +0500

    Add AnimatedSlider to WasabiAutoSpend screen

--- src/components/screens/wallet/SifirWasabiAutoSpendScreen.js
+++ src/components/screens/wallet/SifirWasabiAutoSpendScreen.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {useCallback, useState, useEffect} from 'react';
+import React, {useCallback, useMemo, useState, useEffect} from 'react';
 import {
   View,
   Text,
@@ -7,13 +7,33 @@ import {
   TouchableOpacity,
   StatusBar,
   Image,
+  Animated,
 } from 'react-native';
 import SifirAutoSpendHeader from '@elements/SifirHeaders/SifirAutoSpendHeader';
-import {AppStyle, svg, Images} from '@common';
+import AnimatedSlider from '@elements/AnimatedSlider';
+import {AppStyle, C, Images} from '@common';
 import SifirCard from '../../elements/SifirCard';
 import AnimatedOverlay from '../../elements/AnimatedOverlay';
 import {ScrollView} from 'react-native-gesture-handler';
 import Androw from 'react-native-androw';
+import {scaleLinear} from 'd3-scale';
+import {log} from '@io/events';
+import * as path from 'svg-path-properties';
+import * as shape from 'd3-shape';
+import debounce from '../../../helpers/debounce';
+
+const d3 = {
+  shape,
+};
+
+const slider = React.createRef();
+const label = React.createRef();
+const SV = React.createRef();
+const x = new Animated.Value(0);
+const verticalPadding = 5;
+const width = C.SCREEN_WIDTH - 20;
+const height = 70;
+const cursor = React.createRef();
 
 const listItems = [
   {
@@ -42,6 +62,41 @@ const listItems = [
   },
 ];
 
+const sampleData = {
+  instanceId: null,
+  unspentcoins: [
+    {
+      txid: '1473967d81f9032ea8421bf6fa45688ae7772246ff4a37c0892f75ab7bb36e99',
+      index: 1,
+      amount: 10872,
+      anonymitySet: 42,
+      confirmed: true,
+      label: '',
+      keyPath: "84'/0'/0'/1/13420",
+      address: 'tb1qul3w3n9p87a683z759l5tsllpkazlskz7wmdwm',
+    },
+    {
+      txid: '1473967d81f9032ea8421bf6fa4dasdasdasdasdasd92f75ab7bb36e99',
+      index: 1,
+      amount: 10872,
+      anonymitySet: 3,
+      confirmed: true,
+      label: '',
+      keyPath: "84'/0'/0'/1/13420",
+      address: 'tb1qul3w3n9asdsadasdadasdzlskz7wmdwm',
+    },
+    {
+      txid: '1473967d81f9032ea8421bf6fa4dasdasdasdasdasd92f75ab7bb36e99',
+      index: 1,
+      amount: 10872,
+      anonymitySet: 3,
+      confirmed: true,
+      label: '',
+      keyPath: "84'/0'/0'/1/13420",
+      address: 'tb1qul3w3n9asdsadasdadasdzlskz7wmdwm',
+    },
+  ],
+};
 const SifirWasabiAutoSpendScreen = props => {
   const [isSwitchOn, setSwitchOn] = useState(null);
   const [headerHeight, setHeaderHeight] = useState(0);
@@ -50,7 +105,9 @@ const SifirWasabiAutoSpendScreen = props => {
   const [listContainerPosition, setListContainerPosition] = useState(0);
   const [topTextPosition, setTopTextPosition] = useState(0);
   const [SVoffset, setSVoffset] = useState(0);
+  const [anonset, setanonset] = useState(0);
   const {navigation} = props;
+
   useEffect(() => {
     StatusBar.setBackgroundColor(AppStyle.backgroundColor);
   }, []);
@@ -65,6 +122,115 @@ const SifirWasabiAutoSpendScreen = props => {
   const onLayoutListItem = (event, id) => {
     const {width, height, x, y} = event.nativeEvent.layout;
     setListItemPositions({...listItemPositions, [id]: {width, height, x, y}});
+  };
+
+  const makeUnspentCoinsChartData = chartData => {
+    const data = chartData.reduce((g, t) => {
+      g[Math.floor(t.anonymitySet)] =
+        (g[Math.floor(t.anonymitySet)] || 0) + t.amount;
+      return g;
+    }, {});
+    const sortedAnonsetTotalPairs = Object.entries(data).sort(
+      ([anonset1], [anonset2]) => anonset1 - anonset2,
+    );
+    if (sortedAnonsetTotalPairs.length < 2) {
+      sortedAnonsetTotalPairs.unshift(sortedAnonsetTotalPairs[0]);
+    }
+    const chartStats = sortedAnonsetTotalPairs.reduce(
+      (stats, [anonset, total], i) => {
+        const cumTotal = sortedAnonsetTotalPairs
+          .slice(i)
+          .reduce((totalToIndex, [, t1]) => totalToIndex + t1, 0);
+        stats.series.push([Number(anonset), cumTotal]);
+        stats.maxY = Math.max(cumTotal, stats.maxY);
+        stats.maxX = Math.max(anonset, stats.maxX);
+        stats.minY = Math.min(cumTotal, stats.minY);
+        stats.minX = Math.min(anonset, stats.minX);
+        return stats;
+      },
+      {series: [], minX: 99999, maxX: null, minY: null, maxY: null},
+    );
+    return chartStats;
+  };
+
+  const plotData = sampleData.unspentcoins;
+
+  useEffect(() => {
+    if (selectedWallet?.id) {
+      _init();
+    }
+  }, [selectedWallet]);
+
+  const {series, minX, maxX, minY, maxY} = useMemo(
+    () => makeUnspentCoinsChartData(plotData),
+    [plotData],
+  );
+
+  const {scaleX, scaleY, line, properties, lineLength} = useMemo(() => {
+    const scaleX = scaleLinear()
+      .domain([minX, maxX])
+      .range([20, width - 20]);
+    const scaleY = scaleLinear()
+      .domain([minY, maxY])
+      .range([height - verticalPadding, verticalPadding]);
+    const line = d3.shape
+      .line()
+      .x(([anonset]) => scaleX(Number(anonset)))
+      .y(([, balance]) => scaleY(balance))
+      .curve(d3.shape.curveStepBefore)(series);
+    const p = path.svgPathProperties(line);
+    return {
+      scaleX,
+      scaleY,
+      line,
+      properties: p,
+      lineLength: p.getTotalLength(),
+    };
+  }, [series]);
+
+  const _init = () => {
+    try {
+      x.addListener(({value}) => moveCursor(value));
+      log('Chart data lineLength', lineLength);
+      let {x: X, y} = properties.getPointAtLength(lineLength);
+      let left = X - 10;
+      cursor?.current?.setNativeProps({
+        left,
+      });
+      slider?.current.setNativeProps({
+        left: left - 10,
+      });
+      moveCursor(0);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChartSlider = data =>
+    debounce(({anonset, value}) => setanonset(Math.floor(anonset)), 1);
+
+  const moveCursor = value => {
+    let {x, y} = properties.getPointAtLength(lineLength - value);
+    let left = x - +10;
+    cursor?.current?.setNativeProps({
+      left,
+    });
+    slider?.current.setNativeProps({
+      left: left - 10,
+    });
+    const anonSetValue = scaleX.invert(x);
+    const [, cumSumBalanceValue] = series.find(
+      ([anonset]) => anonSetValue <= anonset,
+    );
+    const text = `${Math.floor(anonSetValue)}`;
+    label?.current?.setNativeProps({
+      text,
+      left: left + 3.5,
+    });
+    handleChartSlider({
+      anonset: anonSetValue,
+      value: cumSumBalanceValue,
+    });
   };
 
   return (
@@ -86,7 +252,6 @@ const SifirWasabiAutoSpendScreen = props => {
       </Text>
       <View style={styles.seperator} />
       <ScrollView
-        // contentContainerStyle={{paddingBottom:  '70%'}}
         onScroll={event => setSVoffset(event.nativeEvent.contentOffset.y)}
         onLayout={event =>
           setListContainerPosition(event.nativeEvent.layout.y)
@@ -168,6 +333,18 @@ const SifirWasabiAutoSpendScreen = props => {
       {selectedWallet?.id && (
         <Androw style={styles.shadow}>
           <View style={styles.stickyContainer}>
+            <View>
+              <AnimatedSlider
+                SV={SV}
+                lineLength={lineLength}
+                slider={slider}
+                label={label}
+                x={x}
+                cursor={cursor}
+                minX={minX}
+                maxX={maxX}
+              />
+            </View>
             <TouchableOpacity style={styles.confirmBtn}>
               <Text style={styles.confirmLabel}>CONFIRM</Text>
             </TouchableOpacity>
